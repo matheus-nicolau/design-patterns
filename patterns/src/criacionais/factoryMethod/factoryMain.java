97d398c2028537be4eea103d531d041d79c630a3
matheus-nicolau <matheusnicolau.dev@gmail.com>
2024-09-21 11:12:54 -0400

    feat: Implements the initial project structure.

---
+++ patterns/src/criacionais/factoryMethod/factoryMain.java
@@ -0,0 +1,10 @@
+package criacionais.factoryMethod;
+
+public class factoryMain {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ patterns/src/criacionais/factoryMethod/productInterface.java
@@ -0,0 +1,5 @@
+package criacionais.factoryMethod;
+
+public interface productInterface {
+
+}
